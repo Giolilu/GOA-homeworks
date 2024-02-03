7bf282e074357b1bc98be3b727de0a7a4106dae0
Giolilu <giolilu10@gmail.com>
2024-02-03 23:17:15 +0400

    first commit

---
+++ day 1/lesson 0 .py
@@ -0,0 +1,69 @@
+print("day 0 homework")
+from turtle import *
+
+
+
+
+# we want to paint house
+
+# first step: draw a squar
+width(7)
+color("red")
+speed(10)
+forward(200)
+
+left(angle=90)
+forward(200)
+left(angle=90)
+forward(200)
+left(angle=90)
+forward(200)
+# door
+left(angle=90)
+forward(75)
+left(angle=90)
+forward(120)
+right(angle=90)
+forward(69)
+right(angle=90)
+forward(120)
+
+# roof
+
+left(angle=90)
+forward(56)
+left(angle=90)
+forward(200)
+color("yellow")
+left(angle=50)
+forward(135)
+left(angle=81)
+forward(135)
+color("red")
+left(angle=50)
+forward(70)
+left(angle=90)
+forward(60)
+left(angle=90)
+forward(55)
+left(angle=90)
+forward(55)
+left(angle=180)
+forward(23)
+right(angle=90)
+forward(55)
+
+
+
+
+
+
+
+exitonclick()
+
+
+
+
+
+
+
